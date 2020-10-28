d590924e6147e1fab54eeb091715bea4b99422f5
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-10-28 15:00:54 +0100

    js-html-css-revolut
    Senza JS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Descrizione:
+//
+// - Focus è rendere funzionali i menu dropdown all’interno della navigazione principale
+// - Importante l’uso di $(this)
+
+
+  console.log($);
+  console.log('js');
